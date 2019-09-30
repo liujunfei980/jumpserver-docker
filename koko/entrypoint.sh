f12b6da16c20ff6a0657524215cfa37d5959e3b9
unknown <liukui@ztgame.com>
2019-09-30 11:09:47 +0800

    upload

---
+++ koko/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+
+sleep 5s
+while [ "$(curl -I -m 10 -o /dev/null -s -w %{http_code} $CORE_HOST)" != "302" ]
+do
+    echo "wait for jms_core ready"
+    sleep 2
+done
+
+if [ ! -f "/opt/coco/config.yml" ]; then
+    cp /opt/coco/config_example.yml /opt/coco/config.yml
+    sed -i '5d' /opt/coco/config.yml
+    sed -i "5i CORE_HOST: $CORE_HOST" /opt/coco/config.yml
+    sed -i "s/BOOTSTRAP_TOKEN: <PleasgeChangeSameWithJumpserver>/BOOTSTRAP_TOKEN: $BOOTSTRAP_TOKEN/g" /opt/coco/config.yml
+    sed -i "s/# LOG_LEVEL: INFO/LOG_LEVEL: ERROR/g" /opt/coco/config.yml
+fi
+
+cd /opt/coco
+./koko
